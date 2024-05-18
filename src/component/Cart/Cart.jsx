2a12859f844730cfce79a07fd09ba970984fd3ab
Galib Mia <gm71bd@gmail.com>
2024-05-18 19:15:23 +0600

    Show data from cart

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -9,28 +9,34 @@ const Cart = (props) => {
     // console.log(cart)
     let total =0;
     let totalShipping = 0;
-    
+    let quantity =0;
 
     for(const product of cart){
-        total = total + product.price;
-        totalShipping = totalShipping + product.shipping;
+        total = total + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping *product.quantity;
+        quantity = quantity + product.quantity;
     }
     const tax = (total * 0.15);
     const totalTax = parseFloat(tax.toFixed(2));
     const grandTotal = total + totalShipping + totalTax;
 
+    const clearCart = () =>{
+        window.localStorage.clear();
+        window.location.reload();
+    }
+
     return (
         <div className='order-summary'>
             <div>
             <h3 className='title'>Order Summary</h3>
-            <p>Total Item Selected: {cart.length}</p>
+            <p>Total Item Selected: {quantity}</p>
             <p>Total Price: ${total}</p>
             <p>Total Shipping Charge: ${totalShipping}</p>
             <p>Tax: ${totalTax}</p>
             <h4>Grand Total: ${grandTotal} </h4>
             </div>
             <div className='btn-container'>
-                <button className='btn-clear'>Clear Cart <FontAwesomeIcon icon={faTrash} /></button>
+                <button onClick={clearCart} className='btn-clear'>Clear Cart <FontAwesomeIcon icon={faTrash} /></button>
                 <button className='btn-review-order'>Review Order <FontAwesomeIcon icon={faArrowRight} /></button>
             </div>
         </div>
